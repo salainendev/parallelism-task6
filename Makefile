6331d411fd4ffbd11861c91621ca367ef550808c
kirill <skirill@gmail.com>
2024-05-01 10:58:35 +0000

    отладил свап вроде бы

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-all:
-	g++ task6.cpp -o task6
+cpu:
+	pgc++ -acc=host -Minfo=accel -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -o task6_host task6.cpp
+gpu:
+	pgc++ -acc=gpu -Minfo=accel -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -o task6_gpu task6.cpp
--- task6.cpp
+++ task6.cpp
@@ -2,6 +2,44 @@
 #include <boost/program_options.hpp>
 #include <cmath>
 #include <memory>
+#include </opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/nvtx3/nvToolsExt.h>
+
+
+template <class ctype> class Data
+    {
+      private:
+        /// Length of the data array
+        int len;
+        /// Data array
+    
+      public:
+        ctype *arr;
+        /// Class constructor
+        Data(int length)
+        {
+          len = length;
+          arr = new ctype[len];
+    #pragma acc enter data copyin(this)
+    #pragma acc enter data create(arr[0:len])
+        }
+
+       
+        
+
+        /// Class destructor
+        ~Data()
+        {
+    #pragma acc exit data delete(arr)
+    #pragma acc exit data delete(this)
+          delete arr;
+          len = 0;
+        }
+    };
+
+
+
+
+
 
 
 // собственно возвращает значение линейной интерполяции
@@ -11,126 +49,128 @@ double linearInterpolation(double x, double x1, double y1, double x2, double y2)
 }
 
 // эта функция берёт матрицу, проходит по её элементам и обновляет их на основе среднего по 4-м соседям , заносит в новую матрицу считает разницу между новым значением
-double oneIteraton(std::unique_ptr<double[]>& prevmatrix,std::unique_ptr<double[]>& curmatrix,int N){
+double oneIteraton(Data<double>& prevmatrix,Data<double>& curmatrix,int N){
     
     double error = 0.0;
+      // Копируем данные на устройство
+    #pragma acc enter data copyin(prevmatrix.arr[0:N*N], curmatrix.arr[0:N*N])
 
+    // Обновляем данные на устройстве перед выполнением операций
+    #pragma acc update device(prevmatrix.arr[0:N*N], curmatrix.arr[0:N*N])
     
-    #pragma acc parallel loop reduction(max:error) present(prevmatrix,curmatrix)
+
+    #pragma acc parallel loop reduction(max:error)
     for (size_t i = 1; i < N-1; i++)
     {
+        #pragma acc loop
         for (size_t j = 1; j < N-1; j++)
         {
-            curmatrix[i*N+j]  = 0.25 * (prevmatrix[i*N+j+1] + prevmatrix[i*N+j-1] + prevmatrix[(i-1)*N+j] + prevmatrix[(i+1)*N+j]);
-            error = std::max(error,std::abs(curmatrix[i*N+j] - prevmatrix[i*N+j]));
+            curmatrix.arr[i*N+j]  = 0.25 * (prevmatrix.arr[i*N+j+1] + prevmatrix.arr[i*N+j-1] + prevmatrix.arr[(i-1)*N+j] + prevmatrix.arr[(i+1)*N+j]);
+            error = fmax(error,fabs(curmatrix.arr[i*N+j] - prevmatrix.arr[i*N+j]));
         }
     }
 
-    
-    #pragma acc parallel loop present(prevmatrix,curmatrix)
-    for (size_t i = 1; i < N-1; i++)
-    {
-        for (size_t j = 1; j < N-1; j++)
-        {
-            prevmatrix[i*N+j] = curmatrix[i*N+j];
-        }
-    }
+    // Обновляем данные на хосте после выполнения операций
+    #pragma acc update self(curmatrix.arr[0:N*N])
+
+    // Освобождаем память на устройстве
+    #pragma acc exit data delete(prevmatrix.arr[0:N*N], curmatrix.arr[0:N*N])
+
+
     return error;
 }
-
-
-
-
-
-
-int main(int argc, char const *argv[])
-{
-    int N = 16;
-    int count_iteration = 10;
-    double precision  =  1e-6;
-    std::unique_ptr<double[]> prevmatrix(new double[N*N]); // матрица температуры на предыдущем шаге
-    std::unique_ptr<double[]> curmatrix(new double[N*N]); // матрица температуры на текущем шаге
-    // заполнение матрицы нулями
+void initMatrices(Data<double>& curmatrix,int N){
     for (size_t i = 0; i < N; i++)
     {
         for (size_t j = 0; j < N; j++)
         {
-            curmatrix[i*N + j] = 0;
+            curmatrix.arr[i*N + j] = 0;
         }
         
     }
-    for (size_t i = 0; i < N; i++)
-    {
-        for (size_t j = 0; j < N; j++)
-        {
-            curmatrix[i*N + j] = 0;
-        
-        }
-    }
-    // заполнение углов матрицы по очереди ( как я понял по часовой стрелке)
-    curmatrix[0] = 10.0;
-    curmatrix[N-1] = 20.0;
-    curmatrix[(N-1)*N + (N-1)] =30.0;
-    curmatrix[(N-1)*N] = 20.0; 
-    // линейная интерполяция между углами матриц 
-    
-    // верхняя граница 
+    curmatrix.arr[0] = 10.0;
+    curmatrix.arr[N-1] = 20.0;
+    curmatrix.arr[(N-1)*N + (N-1)] = 30.0;
+    curmatrix.arr[(N-1)*N] = 20.0;
+    // up граница
     for (size_t i = 1; i < N-1; i++)
     {
-        curmatrix[0*N+i] = linearInterpolation(i,0.0,curmatrix[0],N-1,curmatrix[N-1]);
+        curmatrix.arr[0*N+i] = linearInterpolation(i,0.0,curmatrix.arr[0],N-1,curmatrix.arr[N-1]);
     }
     // left граница 
     for (size_t i = 1; i < N-1; i++)
     {
-        curmatrix[i*N+0] = linearInterpolation(i,0.0,curmatrix[0],N-1,curmatrix[(N-1)*N]);
+        curmatrix.arr[i*N+0] = linearInterpolation(i,0.0,curmatrix.arr[0],N-1,curmatrix.arr[(N-1)*N]);
     }
     // right граница
 
     for (size_t i = 1; i < N-1; i++)
     {
-        curmatrix[i*N+(N-1)] = linearInterpolation(i,0.0,curmatrix[N-1],N-1,curmatrix[(N-1)*N + (N-1)]);
+        curmatrix.arr[i*N+(N-1)] = linearInterpolation(i,0.0,curmatrix.arr[N-1],N-1,curmatrix.arr[(N-1)*N + (N-1)]);
     }
     
     // нижняя граница 
     for (size_t i = 1; i < N-1; i++)
     {
-        curmatrix[(N-1)*N+i] = linearInterpolation(i,0.0,curmatrix[(N-1)*N],N-1,curmatrix[(N-1)*N + (N-1)]);
+        curmatrix.arr[(N-1)*N+i] = linearInterpolation(i,0.0,curmatrix.arr[(N-1)*N],N-1,curmatrix.arr[(N-1)*N + (N-1)]);
     }
+
+}
+
+void swap(Data<double>& curmatrix, Data<double>& prevmatrix, int N){
+    
+    #pragma acc enter data copyin(curmatrix.arr[0:N*N])
+
+    #pragma acc update device(curmatrix.arr[0:N*N])
+
+    double* curData = curmatrix.arr;
+    double* prevData = prevmatrix.arr;
+
+    
+    std::copy(curData, curData + N*N, prevData);
+
+    
+       // Обновляем данные в prevmatrix на устройстве
+    #pragma acc update device(prevmatrix.arr[0:N*N])
+
+    // Обновляем данные в prevmatrix на хосте
+    #pragma acc update self(prevmatrix.arr[0:N*N])
+
+    // Освобождаем память, занятую для curmatrix.arr на устройстве
+    #pragma acc exit data delete(curmatrix.arr)
     
 
-    // проинициализировали начальный уровень бытия матриц 
-    // перед началом алгоритма предыдущая матрица равна текущей поэтому копируем её значения 
+}
 
+int main(){
+    int N = 16;
+    int accuracy = 1e-6;
+    int count_iter = 100;
+    Data<double> curmatrix(N*N);
+    initMatrices(std::ref(curmatrix),N);
+    Data<double> prevmatrix(N*N);
+    swap(std::ref(curmatrix),std::ref(prevmatrix),N);
+   
     for (size_t i = 0; i < N; i++)
     {
         for (size_t j = 0; j < N; j++)
         {
-            prevmatrix[i*N + j] = curmatrix[i*N + j];
+            /* code */
+            std::cout << curmatrix.arr[i*N+j] << ' ';
+            
         }
-        
+        std::cout << std::endl;
     }
-    // сколько прошло итераций 
-    int iter = 0;
-    double error= 9999.0;
-
-    while (iter < count_iteration && error>precision){
-        error = oneIteraton(std::ref(prevmatrix),std::ref(curmatrix),N);
-        iter++;
-        std::cout << "iteration: " << iter << " " << "precision: " << error << std::endl;
-    }
-    // реализован продвинутый уровень бытия матриц 
-
-    // это для вывода матрицы на экран (тест)
     for (size_t i = 0; i < N; i++)
     {
         for (size_t j = 0; j < N; j++)
         {
-            std::cout << curmatrix[i*N+j] << " ";
+            /* code */
+            std::cout << prevmatrix.arr[i*N+j] << ' ';
+            
         }
-       std::cout << std::endl;
+        std::cout << std::endl;
     }
     
-    
 
-    return 0;
 }

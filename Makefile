3a6a85d3278aba5a87b4bb2801219aae4bb51c2f
kirill <skirill@gmail.com>
2024-05-01 14:56:09 +0000

    aaaaa

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+ARGS = -Minfo=all -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -lboost_program_options
+
+all:
+	make cpu
+	make gpu
+	make multi
+
 cpu:
-	pgc++ -acc=host -Minfo=accel -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -o task6Host task6.cpp
+	pgc++ -acc=host $(ARGS) -o $@ task6.cpp
 gpu:
-	pgc++ -acc=gpu -Minfo=accel -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -o task6Gpu task6.cpp
+	pgc++ -acc=gpu $(ARGS) -o $@ task6.cpp
 multi:
-	pgc++ -acc=multicore -Minfo=accel -I/opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/ -o task6Multi task6.cpp
+	pgc++ -acc=multicore $(ARGS) -o $@ task6.cpp
+
+clean:all
+	rm cpu gpu multi
--- task6.cpp
+++ task6.cpp
@@ -2,7 +2,13 @@
 #include <boost/program_options.hpp>
 #include <cmath>
 #include <memory>
+#include <fstream>
+#include <iomanip>
 #include </opt/nvidia/hpc_sdk/Linux_x86_64/23.11/cuda/12.3/include/nvtx3/nvToolsExt.h>
+#include <omp.h>
+namespace opt = boost::program_options;
+
+
 
 
 template <class ctype> class Data
@@ -39,6 +45,26 @@ template <class ctype> class Data
 
 
 
+void saveMatrixToFile(const Data<double>& matrix, int N, const std::string& filename) {
+    std::ofstream outputFile(filename);
+    if (!outputFile.is_open()) {
+        std::cerr << "Unable to open file " << filename << " for writing." << std::endl;
+        return;
+    }
+
+    // Устанавливаем ширину вывода для каждого элемента
+    int fieldWidth = 10; // Ширина поля вывода, можно настроить по вашему усмотрению
+
+    // Записываем матрицу в файл с выравниванием столбцов
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            outputFile << std::setw(fieldWidth) << std::fixed << std::setprecision(4) << matrix.arr[i * N + j];
+        }
+        outputFile << std::endl;
+    }
+
+    outputFile.close();
+}
 
 
 
@@ -47,12 +73,10 @@ double linearInterpolation(double x, double x1, double y1, double x2, double y2)
     // делаем значение y(щначение клетки)используя формулу линейной интерполяции
     return y1 + ((x - x1) * (y2 - y1) / (x2 - x1));
 }
+double errorCalc(Data<double>& prevmatrix,Data<double>& curmatrix, int N){
 
-// эта функция берёт матрицу, проходит по её элементам и обновляет их на основе среднего по 4-м соседям , заносит в новую матрицу считает разницу между новым значением
-double oneIteraton(Data<double>& prevmatrix,Data<double>& curmatrix,int N){
-    
     double error = 0.0;
-    // Копируем данные на устройство
+        // Копируем данные на устройство
     #pragma acc enter data copyin(curmatrix.arr[0:N*N], prevmatrix.arr[0:N*N])
 
     // Обновляем данные на устройстве
@@ -65,8 +89,43 @@ double oneIteraton(Data<double>& prevmatrix,Data<double>& curmatrix,int N){
         #pragma acc loop
         for (size_t j = 1; j < N-1; j++)
         {
-            curmatrix.arr[i*N+j]  = 0.25 * (prevmatrix.arr[i*N+j+1] + prevmatrix.arr[i*N+j-1] + prevmatrix.arr[(i-1)*N+j] + prevmatrix.arr[(i+1)*N+j]);
+            
             error = fmax(error,fabs(curmatrix.arr[i*N+j] - prevmatrix.arr[i*N+j]));
+        }
+    }
+    
+
+    // Освобождаем память, занятую для массивов на устройстве
+    #pragma acc exit data delete(curmatrix.arr, prevmatrix.arr)
+
+    return error;
+
+}
+
+
+
+
+
+
+// эта функция берёт матрицу, проходит по её элементам и обновляет их на основе среднего по 4-м соседям , заносит в новую матрицу считает разницу между новым значением
+void oneIteraton(Data<double>& prevmatrix,Data<double>& curmatrix,int N){
+    
+    
+    // Копируем данные на устройство
+    #pragma acc enter data copyin(curmatrix.arr[0:N*N], prevmatrix.arr[0:N*N])
+
+    // Обновляем данные на устройстве
+    #pragma acc update device(curmatrix.arr[0:N*N], prevmatrix.arr[0:N*N])
+    
+
+    #pragma acc parallel loop 
+    for (size_t i = 1; i < N-1; i++)
+    {
+        #pragma acc loop
+        for (size_t j = 1; j < N-1; j++)
+        {
+            curmatrix.arr[i*N+j]  = 0.25 * (prevmatrix.arr[i*N+j+1] + prevmatrix.arr[i*N+j-1] + prevmatrix.arr[(i-1)*N+j] + prevmatrix.arr[(i+1)*N+j]);
+            
         }
     }
 
@@ -77,7 +136,7 @@ double oneIteraton(Data<double>& prevmatrix,Data<double>& curmatrix,int N){
     #pragma acc exit data delete(curmatrix.arr, prevmatrix.arr)
 
 
-    return error;
+    
 }
 void initMatrices(Data<double>& curmatrix,int N){
     for (size_t i = 0; i < N; i++)
@@ -142,45 +201,61 @@ void swap(Data<double>& curmatrix, Data<double>& prevmatrix, int N){
 
 }
 
-int main(){
-    int N = 16;
-    double accuracy = 1e-6;
-    int count_iter = 100;
+int main(int argc, char const *argv[]){
+
+    //парсим аргументы
+    opt::options_description desc("опции");
+    desc.add_options()
+        ("accuracy",opt::value<double>(),"точность")
+        ("cellsCount",opt::value<int>(),"размер матрицы")
+        ("iterCount",opt::value<int>(),"количество операций")
+        ("help","помощь")
+    ;
+
+    opt::variables_map vm;
+
+    opt::store(opt::parse_command_line(argc, argv, desc), vm);
+
+    opt::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+
+    
+    // и это всё было только ради того чтобы спарсить аргументы.......
+
+
+
+    double start = omp_get_wtime();
+    int N = vm["cellsCount"].as<int>();
+    double accuracy = vm["accuracy"].as<double>();
+    int count_iter = vm["iterCount"].as<int>();
     Data<double> curmatrix(N*N);
     initMatrices(std::ref(curmatrix),N);
     Data<double> prevmatrix(N*N);
     swap(std::ref(curmatrix),std::ref(prevmatrix),N);
    
-    // for (size_t i = 0; i < N; i++)
-    // {
-    //     for (size_t j = 0; j < N; j++)
-    //     {
-    //         /* code */
-    //         std::cout << curmatrix.arr[i*N+j] << ' ';
-            
-    //     }
-    //     std::cout << std::endl;
-    // }
-    // for (size_t i = 0; i < N; i++)
-    // {
-    //     for (size_t j = 0; j < N; j++)
-    //     {
-    //         /* code */
-    //         std::cout << prevmatrix.arr[i*N+j] << ' ';
-            
-    //     }
-    //     std::cout << std::endl;
-    // }
     double error = 999.0;
     int iter = 0;
     while (count_iter>iter && error > accuracy)
     {
-        error = oneIteraton(std::ref(prevmatrix),std::ref(curmatrix),N);
+        oneIteraton(std::ref(prevmatrix),std::ref(curmatrix),N);
+        if ((iter+1)%10==0){
+            
+        error = errorCalc(std::ref(prevmatrix),std::ref(curmatrix),N);
+        std::cout << "iteration: "<< iter+1 << ' ' << "error: " <<std::setprecision(8)<< error << std::endl;
+        }
         swap(std::ref(curmatrix),std::ref(prevmatrix),N);
-        std::cout << iter << ' ' << error<< std::endl;
+        
+        
         iter++;
     }
-       for (size_t i = 0; i < N; i++)
+
+    if (N<=13){
+
+    for (size_t i = 0; i < N; i++)
     {
         for (size_t j = 0; j < N; j++)
         {
@@ -190,15 +265,9 @@ int main(){
         }
         std::cout << std::endl;
     }
-    for (size_t i = 0; i < N; i++)
-    {
-        for (size_t j = 0; j < N; j++)
-        {
-            /* code */
-            std::cout << prevmatrix.arr[i*N+j] << ' ';
-            
-        }
-        std::cout << std::endl;
     }
 
+    double end = omp_get_wtime();
+    saveMatrixToFile(std::ref(curmatrix), N , "matrix.txt");
+    std::cout << end - start<<std::endl;
 }
